56a1d0c41a73ab2340e1914032b23392912c7cad
Emilia Trentham <lavernatrentham@gmail.com>
2022-11-02 14:38:32 -0600

    Format document

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,3 +1,8 @@
-console.log("Hello World!");
-var API_KEY = "83464db7448f440f2230d5eae25329a5"
-// MAKE A REQUEST TO API
+let API_Key = "83464db7448f440f2230d5eae25329a5";
+let user_input = "";
+// city search input element
+// doc query selector  
+function fetch_coordimates() {
+  var API_URL = "http://api.openweathermap.org/geo/1.0/direct?q=" + user_input + "&limit=5&appid=" + API_Key
+    fecth(API_URL)
+}
